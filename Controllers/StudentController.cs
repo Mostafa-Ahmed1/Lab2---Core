d85897f82dcebd408edd0cc92e1c87ac2ec20a38
Mostaf Ahmed <80547961+Mostafa-Ahmed1@users.noreply.github.com>
2021-12-01 20:21:13 +0200

    Add project files.

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab2___Core.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lab2___Core.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
